f42edcbeedd8a73dd6c67feb81ca8faa0f0b7ace
yk-a111y <yk19980811@163.com>
2022-06-30 02:34:41 +0100

    docs: update_README

--- src/views/product/list/ListIndex.vue
+++ src/views/product/list/ListIndex.vue
@@ -1,5 +1,22 @@
 <template>
-  <span>商品列表</span>
+  <page-container>
+    <!-- 查询部分 -->
+    <app-card>
+      <template #header>
+        数据筛选
+      </template>
+      <el-form>
+        <el-form-item label="商品分类">
+          <el-select>
+            <el-option
+              label="全部"
+              :value="0"
+            />
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </app-card>
+  </page-container>
 </template>
 
 <script setup lang="ts">
